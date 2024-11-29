afdc87125ccd6c8a0b46a7d5e70cd1415cfaf7ea
Mayank Digarse <mayankdigarse3@gmail.com>
2024-11-29 20:37:03 +0530

    sec_set

--- innoback/settings.py
+++ innoback/settings.py
@@ -142,16 +142,26 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 CORS_ALLOWED_ORIGINS = [
-
+    "https://innoadmin.vercel.app",
+    "https://innoadmin.vercel.app/"
+    "https://innocomm.vercel.app",
+    "https://innocomm.vercel.app/",
     os.environ.get("ALLOWED_ORIGIN_DATA")
 
 ]
 CORS_ORIGIN_WHITELIST = [
-
+    "https://innoadmin.vercel.app",
+    "https://innoadmin.vercel.app/"
+    "https://innocomm.vercel.app",
+    "https://innocomm.vercel.app/",
     os.environ.get("ALLOWED_ORIGIN_DATA"),
 ]
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://innoadmin.vercel.app",
+    "https://innoadmin.vercel.app/"
+    "https://innocomm.vercel.app",
+    "https://innocomm.vercel.app/",
     os.environ.get("ALLOWED_ORIGIN_DATA")]
 
 CORS_ALLOW_CREDENTIALS = True
